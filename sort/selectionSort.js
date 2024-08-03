f4e0a6fbfa5aad60de088249e1d52b6d041e8941
Md Sabah Uddin Khan <md.sabah.uddin.khan@sap.com>
2024-08-03 22:36:40 +0530

    b

--- sort/selectionSort.js
+++ sort/selectionSort.js
@@ -1,22 +1,22 @@
-function selectionSortDescending(arr) {
+function selectionSort(arr) {
     let n = arr.length;
-
+    
     for (let i = 0; i < n - 1; i++) {
-        // Assume the maximum is the first element in the unsorted portion
-        let maxIndex = i;
+        // Assume the minimum is the first element in the unsorted portion
+        let minIndex = i;
 
-        // Find the index of the maximum element in the unsorted portion
+        // Find the index of the minimum element in the unsorted portion
         for (let j = i + 1; j < n; j++) {
-            if (arr[j] > arr[maxIndex]) {
-                maxIndex = j;
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
 
-        // Swap the found maximum element with the first element of the unsorted portion
-        if (maxIndex !== i) {
+        // Swap the found minimum element with the first element of the unsorted portion
+        if (minIndex !== i) {
             let temp = arr[i];
-            arr[i] = arr[maxIndex];
-            arr[maxIndex] = temp;
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
     }
 
@@ -26,4 +26,4 @@ function selectionSortDescending(arr) {
 // Example usage
 let array = [4, 1, 8, 3, 9];
 console.log("Original array:", array);
-console.log("Sorted array (Descending):", selectionSortDescending(array));
+console.log("Sorted array:", selectionSort(array));
